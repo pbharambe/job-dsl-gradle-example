b8eeccda90ecfeb90df69281aa4bf5a8f1e0405c
pbharambe <pankaj.bharambe@gmail.com>
2020-04-12 10:06:41 +0530

    Create BaseConfig.groovy

---
+++ src/main/groovy/com/test/ci/BaseConfig.groovy
@@ -0,0 +1,17 @@
+package com.test.ci
+
+class BaseConfig {
+    private static instance = null
+
+    private BaseConfig() {}
+
+    public static BaseConfig GetInstance() {
+        if (instance == null) {
+            instance = new BaseConfig()
+        }
+        return instance
+    }
+
+    String jobFolderName = "Test"
+    String jobFolderPrefix = "${jobFolderName}/"
+}
